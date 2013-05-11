a27359fdfbf3f840ceddb95af6c4647973dbbcc9
Pierre-Alexandre Meyer <pierre@ning.com>
2013-05-10 17:47:02 -0700

    build: add release script
    Signed-off-by: Pierre-Alexandre Meyer <pierre@ning.com>

---
+++ release.sh
@@ -0,0 +1,27 @@
+set -e
+
+VERSION=`grep -E '<version>([0-9]+\.[0-9]+\.[0-9]+)</version>' pom.xml | sed 's/[\t \n]*<version>\(.*\)<\/version>[\t \n]*/\1/'`
+if [ "$VERSION" != "$(cat $PWD/VERSION)" ]; then
+  echo "Unable to release: make sure the versions in pom.xml and VERSION match"
+  exit 1
+fi
+
+echo "Cleaning up"
+rake killbill:clean ; rake build
+
+echo "Pushing the gem to Rubygems"
+rake release
+
+echo "Building artifact"
+rake killbill:package
+
+ARTIFACT="$PWD/pkg/killbill-payment-test-$VERSION.tar.gz"
+echo "Pushing $ARTIFACT to Maven Central"
+mvn gpg:sign-and-deploy-file \
+    -DgroupId=com.ning.killbill.ruby \
+    -DartifactId=payment-test-plugin \
+    -Dversion=$VERSION \
+    -Dpackaging=tar.gz \
+    -DrepositoryId=ossrh-releases \
+    -Durl=https://oss.sonatype.org/service/local/staging/deploy/maven2/ \
+    -Dfile=$ARTIFACT
